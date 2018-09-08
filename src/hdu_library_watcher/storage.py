c1aa3ca3b7f1bb2ce5400f380547cea5a7ddd3f0
WizardMeow <goldenkingppp@gmail.com>
2018-09-08 21:52:52 +0800

    Clean Up

---
+++ src/hdu_library_watcher/storage.py
@@ -0,0 +1,42 @@
+import json
+import os
+import time
+import typing
+
+from src.hdu_library_watcher.book import Book
+
+path = 'book.json'
+
+
+class FLock:
+    def __init__(self, block=True):
+        self.block = block
+
+    def __enter__(self):
+        if self.block:
+            while True:
+                if not os.path.exists('LOCK'):
+                    break
+                print('wait for lock')
+                time.sleep(1)
+        else:
+            if os.path.exists('LOCK'):
+                raise BlockingIOError
+        open('LOCK', 'w').close()
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        os.remove('LOCK')
+
+
+def dump(store: typing.Dict[str, 'Book']):
+    with open(path, 'w') as f:
+        json.dump(store, f, default=Book.serialize)
+
+
+def load() -> typing.Dict[str, 'Book']:
+    try:
+        with open(path, 'r') as f:
+            store = json.load(f, object_hook=Book.deserialization)
+    except FileNotFoundError:
+        store = {}
+    return store
